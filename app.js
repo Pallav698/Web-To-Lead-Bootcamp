3a3ab807ba5a34412e26f559d9760ce832a5e906
Pallav <iampallav698@gmail.com>
2024-03-30 17:55:51 +0530

    fixed captchaIssues

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-function beforeSubmit(){
-    let outputDate = document.querySelector(".outputDate");
-    let inputDate = document.querySelector(".inputDate");
-    console.log(inputDate.value);
-    
-    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
-    outputDate.value = formattedDate;
+let captchaChecked = false;
+function beforeSubmit(event){
+    if(captchaChecked) {
+        let outputDate = document.querySelector(".outputDate");
+        let inputDate = document.querySelector(".inputDate");
+
+        let formattedDate = new Date(inputDate.value).toLocaleDateString(
+          "en-IN"
+        );
+        outputDate.value = formattedDate;
+    }
+    else{
+        alert("Please check the captcha");
+        event.event.preventDefault();
+    }
+}
+
+function captchasuccess() {
+    this.captchaChecked = true;
 }
